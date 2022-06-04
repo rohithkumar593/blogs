ff3f6fd0bcfbf2bc5b20699d260027aa4bfb11ce
rohithkumar593 <54279129+rohithkumar593@users.noreply.github.com>
2022-06-04 10:07:01 +0530

    Update settings.py

--- blogs/blogs/settings.py
+++ blogs/blogs/settings.py
@@ -126,10 +126,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_URL= 'two_factor:login'
 EMAIL_HOST_USER="rohithkumar593@gmail.com"
 EMAIL_HOST_PASSWORD=""
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
